407bdc1eb4d257979c6d9628339e025a421df037
manish reddy <reddymanish212@gmail.com>
2026-02-14 09:32:14 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("This is manishreddy at codegnan")
+print("Today,I learn introduction about python")
+print("python is a basic foundation for everthing")
+print("python was developed guido van rossum")
